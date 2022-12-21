a85157530c0003ab2addf9fd5d85b908f3fecf05
Jarrod Wandrey <37374270+JarrodWandrey@users.noreply.github.com>
2022-12-21 12:04:07 -0600

    Delete TheSite/src directory

--- TheSite/src/app.js
+++
@@ -1,8 +0,0 @@
-const hamburerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
-
-function toggleButton() {
-    navList.classList.toggle('show')
-}
-
-hamburerButton.addEventListener('click', toggleButton)
